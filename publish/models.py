d0cc81b2cc38b95bc37e78513760897aab2f3763
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-05-26 01:52:48 +0800

    创建User模型测试

--- publish/models.py
+++ publish/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username

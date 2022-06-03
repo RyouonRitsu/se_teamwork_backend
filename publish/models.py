736f032c772cb29513176739ff1baed849c6d5d6
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-06-04 01:42:17 +0800

    完善注册登录

--- publish/models.py
+++ publish/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     is_admin = models.BooleanField(default=False)
     is_banned = models.BooleanField(default=False)
-    email = models.EmailField(blank=True)
+    email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=20, blank=True)
     city = models.CharField(max_length=50, blank=True)
     address = models.CharField(max_length=100, blank=True)
--- publish/views.py
+++ publish/views.py
@@ -12,13 +12,18 @@ def register(request):
         username = request.POST.get('username')
         password_1 = request.POST.get('password_1')
         password_2 = request.POST.get('password_2')
-        if len(str(username)) == 0 or len(str(password_1)) == 0 or len(str(password_2)) == 0:
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        city = request.POST.get('city')
+        address = request.POST.get('address')
+        introduction = request.POST.get('introduction')
+        if len(str(username)) == 0 or len(str(password_1)) == 0 or len(str(password_2)) == 0 or len(str(email)) == 0:
             return JsonResponse({'errno': 911, 'msg': '必填字段為空'})
         try:
             _ = User.objects.get(username=username)
             return JsonResponse({'errno': 904, 'msg': '用戶名已存在'})
         except User.DoesNotExist:
-            if re.match('.*[^a-zA-Z\\d]+.*', str(username)) is not None:
+            if re.match('.*\\W+.*', str(username)) is not None or len(str(username)) > 30:
                 return JsonResponse({'errno': 903, 'msg': '用戶名不合法'})
             elif password_1 != password_2:
                 return JsonResponse({'errno': 902, 'msg': '兩次輸入的密碼不同'})
@@ -26,12 +31,26 @@ def register(request):
                     re.match('.*\\d+.*', str(password_1)) is None or \
                     re.match('.*[a-zA-Z]+.*', str(password_1)) is None:
                 return JsonResponse({'errno': 905, 'msg': '密碼不合法'})
+            elif len(str(email)) > 254 or re.match('.*[^a-zA-Z\\d@.]+.*', str(email)) is not None:
+                return JsonResponse({'errno': 907, 'msg': 'Email不合法'})
+            elif len(str(phone)) != 11 or re.match('.*\\D+.*', str(phone)) is not None:
+                return JsonResponse({'errno': 908, 'msg': '手機號碼不合法'})
+            elif len(str(city)) > 50 or len(str(address)) > 100:
+                return JsonResponse({'errno': 909, 'msg': '城市或地址不合法'})
             else:
-                new_author = User(username=username, password=password_1)
-                new_author.save()
+                new_user = User(
+                    username=username,
+                    password=password_1,
+                    email=email,
+                    phone=phone,
+                    city=city,
+                    address=address,
+                    introduction=introduction
+                )
+                new_user.save()
                 return JsonResponse({'errno': 0, 'msg': '註冊成功'})
     else:
-        return JsonResponse({'errno': 901, 'msg': '請求方式錯誤'})
+        return JsonResponse({'errno': 901, 'msg': '請求方式錯誤, 只接受POST'})
 
 
 @csrf_exempt
@@ -42,8 +61,8 @@ def login(request):
         if len(str(username)) == 0 or len(str(password)) == 0:
             return JsonResponse({'errno': 911, 'msg': '必填字段為空'})
         try:
-            author = User.objects.get(username=username)
-            if author.password == password:
+            user = User.objects.get(username=username)
+            if user.password == password:
                 request.session['username'] = username
                 return JsonResponse({'errno': 0, 'msg': '登入成功'})
             else:
@@ -51,7 +70,7 @@ def login(request):
         except User.DoesNotExist:
             return JsonResponse({'errno': 906, 'msg': '用戶不存在'})
     else:
-        return JsonResponse({'errno': 901, 'msg': '請求方式錯誤'})
+        return JsonResponse({'errno': 901, 'msg': '請求方式錯誤, 只接受POST'})
 
 
 @csrf_exempt

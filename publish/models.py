2d7283c465c77246022ba3d60be58bf7d1e08a85
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-06-04 00:39:10 +0800

    重构用户数据库

--- publish/models.py
+++ publish/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+    is_admin = models.BooleanField(default=False)
+    is_banned = models.BooleanField(default=False)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    introduction = models.TextField(blank=True)
 
     def __str__(self):
-        return self.username
+        return f'{self.user_id}: {self.username}'

d4b8bf9f4e879654120794541bf3bb6cfe297a78
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-06-05 05:08:27 +0800

    遷移publish到account

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import *
+from account.models import *
 import re
 
 # Create your views here.

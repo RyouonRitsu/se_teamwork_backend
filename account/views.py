da9a4b0aed76d3a96e85a4f712b54f6f14f53dc5
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-06-05 05:11:56 +0800

    修改註釋描述

--- account/views.py
+++ account/views.py
@@ -8,7 +8,7 @@ import re
 
 """
 調用方法:
-    前端發送GET或POST請求到/publish/函數名, 並添加相應的Params或Body(data使用鍵值對形式), 即可獲得包含相應信息的響應返回
+    前端發送GET或POST請求到/account/函數名, 並添加相應的Params或Body(data使用鍵值對形式), 即可獲得包含相應信息的響應返回
 errno:
     0:      [成功]
     901:    請求方式錯誤, 只接受POST請求

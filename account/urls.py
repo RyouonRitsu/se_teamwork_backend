0fb225bf996f08432a88cd96c4c2ff611c32c76d
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-06-06 23:47:54 +0800

    简化逻辑

--- account/urls.py
+++ account/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('register', register),
     path('login', login),
     path('logout', logout),
-    path('get_user_info_by_username', get_user_info_by_username),
-    path('get_user_info_by_user_id', get_user_info_by_user_id),
     path('get_user_info', get_user_info),
     path('update_user_info', update_user_info)
 ]

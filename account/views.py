9acc8aa0ee40ba2289b49fc0b59071753fbe0008
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-06-07 22:50:59 +0800

    增加检查登陆装饰器函数util

--- account/views.py
+++ account/views.py
@@ -157,7 +157,16 @@ def login(request):
         return JsonResponse({'errno': 901, 'msg': '請求方式錯誤, 只接受POST請求'})
 
 
+def login_required(func):
+    def wrapper(request, *args, **kwargs):
+        if request.session.get('user_id') is None:
+            return JsonResponse({'errno': 912, 'msg': '用戶未登入'})
+        return func(request, *args, **kwargs)
+    return wrapper
+
+
 @csrf_exempt
+@login_required
 def logout(request):
     """
     用户登出, 只接受POST請求, Body為空

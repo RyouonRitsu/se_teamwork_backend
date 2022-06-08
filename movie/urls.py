a590b16b3c843843bf899954e6d3e72be8b6152a
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-06-09 04:25:37 +0800

    urls

--- movie/urls.py
+++ movie/urls.py
@@ -4,4 +4,10 @@ from .views import *
 urlpatterns = [
     # path('url_name', api_name)
     # 这是一个样例，指定路由名为url_name，对应处理函数为当前app内views.py中的api_name
+    path('add_movie', add_movie),
+    path('delete_movie', delete_movie),
+    path('update_movie_info', update_movie_info),
+    path('get_movie_info_by_key', get_movie_info_by_key),
+    path('get_movie_info', get_movie_info),
+    path('get_movie_info_by_id', get_movie_info_by_id)
 ]

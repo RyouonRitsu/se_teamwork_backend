de46acd1c4d25e2f16c1df7c165dafe2042d4225
Nassim <73381393+nassim-suo@users.noreply.github.com>
2022-06-09 23:45:21 +0800

    优化models

--- comment/models.py
+++ comment/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+from django.db.models import ManyToManyField, DateTimeField
 
 
 class Comment(models.Model):
@@ -15,18 +15,22 @@ class Comment(models.Model):
     body_id = models.BigIntegerField(default=0, null=False)
     comment_num_comments = models.IntegerField(default=0)
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'content': self.content,
-            'date': self.date,
-            'num_likes': self.num_likes,
-            'num_dislikes': self.num_dislikes,
-            'authorId': self.authorId,
-            'type': self.type,
-            'body_id': self.body_id,
-            'comment_num_comments': self.comment_num_comments
-        }
+    def to_dict(self, fields=None, exclude=None):
+        data = {}
+        for f in self._meta.concrete_fields + self._meta.many_to_many:
+            value = f.value_from_object(self)
+            if fields and f.name not in fields:
+                continue
+            if exclude and f.name in exclude:
+                continue
+            if isinstance(f, ManyToManyField):
+                value = [i.id for i in value] if self.pk else None
+            if isinstance(f, DateTimeField):
+                value = value.strftime('%Y/%m/%d %H:%M:%S') if value else None
+            if isinstance(f, models.ImageField):
+                value = value.name if value else None
+            data[f.name] = value
+        return data
 
 
 class Report(models.Model):

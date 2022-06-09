bcc5b69d9df014bb373d1a15b9f117ccb8b736e8
Nassim <73381393+nassim-suo@users.noreply.github.com>
2022-06-10 01:24:21 +0800

    迁移数据库

--- comment/models.py
+++ comment/models.py
@@ -36,7 +36,24 @@ class Comment(models.Model):
 class Report(models.Model):
     id = models.BigAutoField(primary_key=True)
     reason = models.TextField(null=False)
-    title = models.TextField(null=False)
+    title = models.TextField(null=False,default='')
     date = models.DateTimeField(auto_now_add=True)
     reporter_id = models.BigIntegerField(default=0, null=False)
     comment_id = models.BigIntegerField(default=0, null=False)
+
+    def to_dict(self, fields=None, exclude=None):
+        data = {}
+        for f in self._meta.concrete_fields + self._meta.many_to_many:
+            value = f.value_from_object(self)
+            if fields and f.name not in fields:
+                continue
+            if exclude and f.name in exclude:
+                continue
+            if isinstance(f, ManyToManyField):
+                value = [i.id for i in value] if self.pk else None
+            if isinstance(f, DateTimeField):
+                value = value.strftime('%Y/%m/%d %H:%M:%S') if value else None
+            if isinstance(f, models.ImageField):
+                value = value.name if value else None
+            data[f.name] = value
+        return data

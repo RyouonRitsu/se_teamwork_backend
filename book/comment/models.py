6daebff48575917d9ea6228d97d3cc183cbfb9a0
Nassim <73381393+nassim-suo@users.noreply.github.com>
2022-06-09 23:06:32 +0800

    返回最高赞评论

---
+++ book/comment/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Comment(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    content = models.TextField(null=False)
+    date = models.DateTimeField(auto_now_add=True)
+    num_likes = models.IntegerField(default=0)
+    num_dislikes = models.IntegerField(default=0)
+    authorId = models.BigIntegerField(default=0, null=False)
+    type = models.IntegerField(default=0, null=False)
+    body_id = models.BigIntegerField(default=0, null=False)
+    comment_num_comments = models.IntegerField(default=0)
+
+
+class Report(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    reason = models.TextField(null=False)
+    title = models.TextField(null=False)
+    date = models.DateTimeField(auto_now_add=True)
+    reporter_id = models.BigIntegerField(default=0, null=False)
+    comment_id = models.BigIntegerField(default=0, null=False)
